041f3216be20f1bd9dede60113cfbb59c135b7f7
Marjan Nikolic <marjan1505@gmail.com>
2017-01-31 16:41:34 +0100

    delete components

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -6,9 +6,17 @@ import { Component } from '@angular/core';
     <h1>Inline Template</h1>
     <fa-other></fa-other>
     <fa-third></fa-third>
-    <fa-another></fa-another>
+    <fa-another>
+      <div>
+        <h1>Hello</h1>
+        <p>World!</p>
+      </div>
+    </fa-another>
+    <fa-another>
+      <p>Something else</p>
+    </fa-another>
   `,
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'I changed it!';
