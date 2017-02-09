619f3f9aac63ad3dc4506e042b4fc903e492bd55
Marjan Nikolic <marjan1505@gmail.com>
2017-02-09 16:11:18 +0100

    new component

--- recipe-book/src/app/shopping-list/shopping-list.component.ts
+++ recipe-book/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ShoppingListAddComponent } from './shopping-list-add.component';
+
 @Component({
   selector: 'rb-shopping-list',
   templateUrl: './shopping-list.component.html'
